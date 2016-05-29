ac2e56bfc696c5a9cbc288aa592eaaa8a37954a1
yanruoxinb <yanruoxinb@users.noreply.github.com>
2016-05-29 18:31:37 +0800

    Update double_dimensional_array_to_one_2.js

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,14 +3,12 @@
 function double_to_one(collection) {
 	var arr=new Array();
 	var outLen=collection.length;
-	for(var i=0;i<len;i++){
+	for(var i=0;i<outLen;i++){
 		var innerLen=collection[i].length;
 		for(var j=0;j<innerLen;j++){
 				if(arr.indexOf(collection[i][j])==-1)
 					arr.push(collection[i][j]);
-					
-		
-	}
+		}
 	}
 	return arr;
 }
