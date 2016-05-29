b47bac1213409bd26859b7fcda394cbcac67ff97
unknown <>
2016-05-29 18:25:51 +0800

    some

---
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -0,0 +1,15 @@
+'use strict';
+
+function double_to_one(collection) {
+
+	var arr=new Array();
+	var outLen=collection.length;
+	for(var i=0;i<outLen;i++){
+		arr.push(collection[i]);
+		
+	}
+	return arr;
+  //在这里写入代码
+}
+
+module.exports = double_to_one;
---
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -0,0 +1,18 @@
+'use strict';
+
+function double_to_one(collection) {
+	var arr=new Array();
+	var outLen=collection.length;
+	for(var i=0;i<len;i++){
+		var innerLen=collection[i].length;
+		for(var j=0;j<innerLen;j++){
+				if(arr.indexOf(collection[i][j])==-1)
+					arr.push(collection[i][j]);
+					
+		
+	}
+	}
+	return arr;
+}
+
+module.exports = double_to_one;
