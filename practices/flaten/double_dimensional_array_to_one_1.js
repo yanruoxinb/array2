4e109988720873fcad95cd1c5b2bc2f0421d0939
yanruoxinb <yanruoxinb@users.noreply.github.com>
2016-05-29 20:15:39 +0800

    Update double_dimensional_array_to_one_1.js

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,19 +1,21 @@
 'use strict';
 
 function double_to_one(collection) {
-var arr=new Array();
+
+		var arr=new Array();
 		var Len=collection.length;
-		for(var i=0;i<Len;i++){			
-			if(isArray(collection[i])==true){
-				var innerLen=collection[i].length
+		for(var i=0;i<Len;i++){
+			if(collection[i].length){
+				var innerLen=collection[i].length;
 				for(var j=0;j<innerLen;j++){
 					arr.push(collection[i][j]);
 				}
 			}
-			else
-				arr.push(collection[i])
+			else{
+				arr.push(collection[i]);	
+			}
 		}
-return arr;
+		return arr;
 }
 
 module.exports = double_to_one;
